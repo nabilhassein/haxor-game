8e417ec9210e5c22588bee3d70703e6dfc95bb9c
Nabil Hassein <nabil.hassein@gmail.com>
2013-08-19 14:39:02 -0400

    changes to automatic provisioning of remote server

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 apt-get update
 
-apt-get install nginx
+apt-get install nginx --assume-yes
 
 service nginx restart
+
+# TODO: figure out server_name in nginx.cnf
+
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
+mv /vagrant/conf/nginx.conf /etc/nginx
+
+nginx -s reload
+
+# /vagrant/bin/server # possible to get game server running not as root?
